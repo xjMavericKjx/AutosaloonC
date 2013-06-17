84a17ae86a1c6910127cb2e1961d74e67e51ec3f
Maksim <maximaxim@tpu.ru>
2013-06-17 18:21:02 +0700

    add project

---
+++ Autosaloon/Autosaloon/Avtosaloon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autosaloon
+{
+    public class Avtosaloon
+    {
+        public string Name { get; set; }
+
+        public Avtosaloon(string name)
+        {
+            Name = name;
+        }
+        private ArrayList Cars= new ArrayList();
+
+
+    }
+}
---
+++ Autosaloon/Autosaloon/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autosaloon
+{
+    public class Car
+    {
+        private string Name;
+        private int MaximumNumberOfPassengers;
+        private int Cost;
+        private int QuantityInStock;
+        private bool Availability;
+        private Avtosaloon Aulotasoon;
+        private ArrayList ApplicationsForPurchase = new ArrayList();
+
+        public Car()
+        {
+
+        }
+
+    }
+}
